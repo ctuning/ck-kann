62f2aba204b640aaa3f1929313dab00dc12a2970
Anton Lokhmotov <anton@dividiti.com>
2017-05-21 02:14:01 +0200

    Use preprocessing to create temporary input.

---
+++ program/kann-sample/preprocess.py
@@ -0,0 +1,39 @@
+#
+# Prepare temporary KaNN input by concatenating several images.
+#
+# Developer(s):
+# - Anton Lokhmotov, dividiti, 2017
+#
+
+import glob
+import os
+import shutil
+
+def ck_preprocess(i):
+
+    ck=i['ck_kernel']
+    deps=i['deps']
+    env=i['env']
+
+    # Get the maximum number of images.
+    max_num_images=env.get('CK_KANN_MAX_NUMBER_IMAGES',1)
+
+    # Create temporary input using random max_num_images from the kann-val dataset.
+    # TODO: Use images with consecutive numbering?
+    imagenet_val=deps['kann-val']
+    imagenet_val_dir=imagenet_val['dict']['env']['CK_ENV_DATASET_IMAGENET_VAL_KANN']
+    imagenet_val_files=glob.glob(imagenet_val_dir+'/ILSVRC2012_val_*.kann_input')
+
+    kann_input_file=os.path.join(os.getcwd(), 'tmp-kann-input.tmp')
+    with open(kann_input_file, 'wb') as kann_input_f:
+        num_images = 0
+        for imagenet_val_file in imagenet_val_files:
+            with open(imagenet_val_file, 'rb') as imagenet_val_f:
+                shutil.copyfileobj(imagenet_val_f, kann_input_f)
+            num_images += 1
+            if num_images >= max_num_images: break
+
+    b=''
+    return {'return':0, 'bat':b, 'new_env':nenv}
+
+# Do not add anything here!
