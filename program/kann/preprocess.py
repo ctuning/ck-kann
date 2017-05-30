265483c09b0ad23a55198581ee8fd0a72a1864e0
Anton Lokhmotov <anton@dividiti.com>
2017-05-30 22:59:31 +0200

    Save names of concatenated input files.

--- program/kann/preprocess.py
+++ program/kann/preprocess.py
@@ -18,7 +18,7 @@ def ck_preprocess(i):
     env=i['env']
 
     # Get the maximum number of images.
-    max_num_images=env.get('CK_KANN_MAX_NUMBER_IMAGES',1)
+    max_num_images=int(env.get('CK_KANN_MAX_NUMBER_IMAGES', 1))
 
     # Create temporary input using random max_num_images from the kann-val dataset.
     # TODO: Use images with consecutive numbering?
@@ -27,11 +27,13 @@ def ck_preprocess(i):
     imagenet_val_files=glob.glob(imagenet_val_dir+'/ILSVRC2012_val_*.kann_input')
 
     kann_input_file=os.path.join(os.getcwd(), 'tmp-kann-input.tmp')
-    with open(kann_input_file, 'wb') as kann_input_f:
+    kann_paths_file=os.path.join(os.getcwd(), 'tmp-kann-paths.tmp')
+    with open(kann_input_file, 'wb') as kann_input_f, open(kann_paths_file, 'w') as kann_paths_f:
         num_images = 0
         for imagenet_val_file in imagenet_val_files:
             with open(imagenet_val_file, 'rb') as imagenet_val_f:
                 shutil.copyfileobj(imagenet_val_f, kann_input_f)
+            kann_paths_f.write(os.path.basename(imagenet_val_file) + '\n')
             num_images += 1
             if num_images >= max_num_images: break
 
